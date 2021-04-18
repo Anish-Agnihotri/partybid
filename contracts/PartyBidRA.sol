c053890f4ae8134209bf07955258c5da28fa7125
Anish Agnihotri <contact@anishagnihotri.com>
2021-04-18 00:16:23 -0400

    Balance tracking fixes

--- contracts/PartyBidRA.sol
+++ contracts/PartyBidRA.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity 0.6.8;
 
+import { ReserveAuctionV3 } from "../reserve-auction-v3/ReserveAuctionV3.sol";
 import { SafeMath } from "../node_modules/@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
 contract PartyBid {
   // Use OpenZeppelin library for SafeMath
   using SafeMath for uint256;
+
+  // ============ Immutable storage ============
+
+  address public immutable ReserveAuctionV3Address;
 
   // ============ Mutable storage ============
 
@@ -22,13 +27,32 @@ contract PartyBid {
   // Stakes of individual dao members
   mapping (address => uint256) public daoStakes;
 
+  // ============ Constructor ============
+
+  constructor(
+    address _ReserveAuctionV3Address,
+    uint256 _auctionID,
+    uint256 _bidAmount,
+    uint256 _exitTimeout
+  ) public {
+    // Initialize immutable memory
+    ReserveAuctionV3Address = _ReserveAuctionV3Address;
+
+    // Initialize mutable memory
+    auctionID = _auctionID;
+    bidAmount = _bidAmount;
+    currentRaisedAmount = 0;
+    exitTimeout = _exitTimeout;
+    bidPlaced = false;
+  }
+
   // ============ Join the DAO ============
 
   /**
    * Join the DAO by sending ETH
    * Requires bidding to be enabled, forced matching of deposit value to sent eth, and there to be capacity in DAO
    */
-  function join(uint256 _value) external {
+  function join(uint256 _value) external payable {
     // Dont allow joining once the bid has already been placed
     require(bidPlaced == false, "PartyBid: Cannot join since bid has been placed.");
     // Enforce matching of deposited value to ETH sent to contract
@@ -36,11 +60,28 @@ contract PartyBid {
     // Enforce sum(eth sent, current raised) <= required bid amount
     require(_value.add(currentRaisedAmount) <= bidAmount, "PartyBid: DAO does not have capacity.");
 
-    currentRaisedAmount += currentRaisedAmount.add(_value); // Increment raised amount
-    daoStakes[msg.sender] += daoStakes[msg.sender].add(_value); // Track DAO member contribution
+    currentRaisedAmount = currentRaisedAmount.add(_value); // Increment raised amount
+    daoStakes[msg.sender] = daoStakes[msg.sender].add(_value); // Track DAO member contribution
   }
 
   // ============ Place a bid from DAO ============
+
+  /**
+   * Execute bid placement, as a DAO member (so long as required conditions are met)
+   */
+  function placeBid() external {
+    // Dont allow placing a bid if already placed
+    require(bidPlaced == false, "PartyBid: Bid has already been placed.");
+    // Ensure that required bidAmount is matched with currently raised amount
+    require(bidAmount == currentRaisedAmount, "PartyBid: Insufficient raised capital to place bid.");
+    // Ensure that caller is a DAO member
+    require(daoStakes[msg.sender] > 0, "PartyBid: Must be DAO member to initiate placing bid.");
+
+    // Setup auction contract, place bid, toggle bidding status
+    ReserveAuctionV3 auction_contract = ReserveAuctionV3(ReserveAuctionV3Address);
+    auction_contract.createBid.value(bidAmount)(auctionID, bidAmount);
+    bidPlaced = true;
+  }
 
   // ============ Accept incoming ReserveAuctionV3 item ============
 
