bfb1847110385c26a988e396a8806312787e437b
Anish Agnihotri <contact@anishagnihotri.com>
2021-04-17 21:36:50 -0400

    Scrappy exit functionality

--- contracts/PartyBidRA.sol
+++ contracts/PartyBidRA.sol
@@ -22,21 +22,35 @@ contract PartyBid {
   // Stakes of individual dao members
   mapping (address => uint256) public daoStakes;
 
+  // ============ Join the DAO ============
+
   /**
-   * Enables joining the DAO
+   * Join the DAO by sending ETH
+   * Requires bidding to be enabled, forced matching of deposit value to sent eth, and there to be capacity in DAO
    */
   function join(uint256 _value) external {
-    require(bidPlaced == false); // dont allow joining once bid is placed
-    require(msg.value == _value); // enforce correct value
-    require(daoStakes[address].add(_value).add(currentRaisedAmount) <= bidAmount);
+    // Dont allow joining once the bid has already been placed
+    require(bidPlaced == false, "PartyBid: Cannot join since bid has been placed.");
+    // Enforce matching of deposited value to ETH sent to contract
+    require(msg.value == _value, "PartyBid: Deposit amount does not match spent ETH.");
+    // Enforce sum(eth sent, current raised) <= required bid amount
+    require(_value.add(currentRaisedAmount) <= bidAmount, "PartyBid: DAO does not have capacity.");
 
-    currentRaisedAmount += currentRaisedAmount.add(_value);
-    daoStakes[msg.sender] += daoStakes[msg.sender].add(_value);
+    currentRaisedAmount += currentRaisedAmount.add(_value); // Increment raised amount
+    daoStakes[msg.sender] += daoStakes[msg.sender].add(_value); // Track DAO member contribution
   }
 
+  // ============ Place a bid from DAO ============
+
+  // ============ Accept incoming ReserveAuctionV3 item ============
+
+  // ============ Bidding + Acceptance functionality for held ReserveAuctionV3 item ============
+
+  // ============ Exit the DAO ============
+  
   function _exitIfBidFailed() internal payable {
-    require(bidPlaced == true); // check if bid was placed
-    require(address(this).balance == bidAmount); // contract should have funds if not top bidder
+    require(bidPlaced == true); // check if bid was placed (double check from caller)
+    require(address(this).balance == bidAmount); // contract should have been returned funds if not top bidder
     require(daoStakes[msg.sender] > 0); // check if individual is a dao member
 
     payable(msg.sender).send(daoStakes[msg.sender]);
@@ -44,7 +58,21 @@ contract PartyBid {
   }
 
   function _exitIfTimeoutPassed() internal payable {
-    require(bidPlaced == false); // check if bid was not placed
+    require(bidPlaced == false); // check if bid was not placed (double check from caller)
     require(block.timestamp >= exitTimeout); // make sure current time > maxTimeout
+    require(daoStakes[msg.sender] > 0); // check if individual is a dao member
+
+    payable(msg.sender).send(daoStakes[msg.sender]);
+    daoStakes[msg.sender] = 0;
+  }
+
+  function exit() external payable {
+    require(daoStakes[msg.sender] > 0); // check if individual is a dao member
+
+    if (bidPlaced) {
+      _exitIfBidFailed();
+    } else {
+      _exitIfTimeoutPassed();
+    }
   }
 }
