971af9209a7ede6a2ce5e38c5d7c580e30b2e034
Anish Agnihotri <contact@anishagnihotri.com>
2021-04-18 02:21:17 -0400

    WIP: post-auction-success re-auctioning

--- contracts/PartyBidRA.sol
+++ contracts/PartyBidRA.sol
@@ -6,11 +6,17 @@ pragma solidity 0.6.8;
 import { ReserveAuctionV3 } from "../reserve-auction-v3/ReserveAuctionV3.sol";
 import { SafeMath } from "../node_modules/@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
-// ============ Interface declerations ============
+// ============ Interface declarations ============
 
+// Wrapped Ether
 interface IWETH {
   function balanceOf(address src) external view returns (uint256);
   function transferFrom(address src, address dst, uint256 wad) external returns (bool);
+}
+
+// ERC721
+interface IERC721 {
+  function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 // @dev: Must use wETH for all outgoing transactions, since returned capital from contract will
@@ -25,6 +31,8 @@ contract PartyBid {
   address public immutable ReserveAuctionV3Address;
   // Address of the wETH contract
   address public immutable wETHAddress;
+  // Address of the NFT contract
+  address public immutable NFTAddress;
 
   // ============ Mutable storage ============
 
@@ -40,19 +48,48 @@ contract PartyBid {
   bool public bidPlaced;
   // Stakes of individual dao members
   mapping (address => uint256) public daoStakes;
+  // List of NFT auction proposals
+  NFTListingProposition[] public NFTListingProposals;
+
+  // ============ Structs ============
+
+  struct NFTListingProposition {
+    address proposer;
+    uint256 reservePrice;
+    uint256 duration;
+    uint256 numSupport;
+  }
+
+  // ============ Modifiers ============
+  
+  // Reverts if the DAO has not won the NFT
+  modifier onlyIfAuctionWon() {
+    // Ensure that owner of NFT(auctionId) is contract address
+    require(IERC721(NFTAddress).ownerOf(auctionID) == address(this), "PartyBid: DAO has not won auction.");
+    _;
+  }
+
+  // ============ Events ============
+
+  // Address of a new DAO member and their entry share
+  event PartyJoined(address indexed, uint256 value);
+  // Value of newly placed bid on ReserveAuctionV3 item
+  event PartyBidPlaced(uint256 auctionID, uint256 value);
+  // Address and exit share of DAO member, along with reason for exit
+  event PartyMemberExited(address indexed, uint256 value, bool postFailure);
 
   // ============ Constructor ============
 
   constructor(
     address _ReserveAuctionV3Address,
-    address _wETHAddress,
     uint256 _auctionID,
     uint256 _bidAmount,
     uint256 _exitTimeout
   ) public {
     // Initialize immutable memory
     ReserveAuctionV3Address = _ReserveAuctionV3Address;
-    wETHAddress = _wETHAddress;
+    wETHAddress = ReserveAuctionV3(_ReserveAuctionV3Address).wethAddress();
+    NFTAddress = ReserveAuctionV3(_ReserveAuctionV3Address).nftContract();
 
     // Initialize mutable memory
     auctionID = _auctionID;
@@ -71,13 +108,15 @@ contract PartyBid {
   function join(uint256 _value) external payable {
     // Dont allow joining once the bid has already been placed
     require(bidPlaced == false, "PartyBid: Cannot join since bid has been placed.");
-    // Enforce matching of deposited value to ETH sent to contract
+    // Ensure matching of deposited value to ETH sent to contract
     require(msg.value == _value, "PartyBid: Deposit amount does not match spent ETH.");
-    // Enforce sum(eth sent, current raised) <= required bid amount
+    // Ensure sum(eth sent, current raised) <= required bid amount
     require(_value.add(currentRaisedAmount) <= bidAmount, "PartyBid: DAO does not have capacity.");
 
     currentRaisedAmount = currentRaisedAmount.add(_value); // Increment raised amount
     daoStakes[msg.sender] = daoStakes[msg.sender].add(_value); // Track DAO member contribution
+
+    emit PartyJoined(msg.sender, _value); // Emit new DAO member
   }
 
   // ============ Place a bid from DAO ============
@@ -97,11 +136,32 @@ contract PartyBid {
     ReserveAuctionV3 auction_contract = ReserveAuctionV3(ReserveAuctionV3Address);
     auction_contract.createBid{value: bidAmount}(auctionID, bidAmount);
     bidPlaced = true;
+
+    emit PartyBidPlaced(auctionID, bidAmount); // Emit bid placed
   }
 
-  // ============ Accept incoming ReserveAuctionV3 item ============
+  // ============ ReserveAuctionV3 NFT handling ============
 
-  // ============ Bidding + Acceptance functionality for held ReserveAuctionV3 item ============
+  /**
+   * Returns boolean status of if DAO has won NFT
+   */
+  function NFTWon() public view returns (bool) {
+    // Check if owner of NFT(auctionID) is contract address
+    return IERC721(NFTAddress).ownerOf(auctionID) == address(this);
+  }
+  
+  function NFTVoteForAuction(uint256 _reservePrice, uint256 duration) external onlyIfAuctionWon() returns (uint256) {
+    NFTListingProposals.push(
+      msg.sender,
+      _reservePrice,
+      duration,
+      daoStakes[msg.sender]
+    );
+  }
+
+  function NFTVoteForAuctionListing() external onlyIfAuctionWon() {}
+
+  function NFTListForAuction() external onlyIfAuctionWon() {}
 
   // ============ Exit the DAO ============
   
@@ -115,8 +175,11 @@ contract PartyBid {
     // Ensure that contract wETH balance is > 0 (implying that either funds have been returned or wETH airdropped)
     require(IWETH(wETHAddress).balanceOf(address(this)) > 0, "PartyBid: DAO bid has not been beaten or refunded yet.");
 
-    // Transfer wETH from contract to DAO member and nullify member DAO share
+    // Transfer wETH from contract to DAO member and emit event
     IWETH(wETHAddress).transferFrom(address(this), msg.sender, daoStakes[msg.sender]);
+    emit PartyMemberExited(msg.sender, daoStakes[msg.sender], true);
+
+    // Nullify member DAO share
     daoStakes[msg.sender] = 0;
   }
 
@@ -129,14 +192,18 @@ contract PartyBid {
     // Ensure that current time > deposit timeout
     require(block.timestamp >= exitTimeout, "PartyBid: Exit timeout not met.");
 
-    // Transfer ETH from contract to DAO member and nullify member DAO share
+    // Transfer ETH from contract to DAO member and emit event
     payable(msg.sender).transfer(daoStakes[msg.sender]);
+    emit PartyMemberExited(msg.sender, daoStakes[msg.sender], false);
+
+    // Nullify member DAO share
     daoStakes[msg.sender] = 0;
   }
 
   /**
    * Public utility function to call internal exit functions based on bid state
    */
+   // TODO: Add exit functionality post-won-NFT sale
   function exit() external payable {
     // Ensure that caller is a DAO member
     require(daoStakes[msg.sender] > 0, "PartyBid: Must first be a DAO member to exit DAO.");
